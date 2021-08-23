802843c73acf114499d3830ee3bdea6282755d4f
Kaushik Donthi <kaushikdr@gmail.com>
2021-08-23 11:37:35 -0400

    removed uneeded code and remove tests

--- app.js
+++ app.js
@@ -53,14 +53,6 @@ personSchema.index({
     tags: 1}, {unique: true}
 );
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", '*');
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-    next();
-});
-
 //Creating model
 const Person = mongoose.model("Person", personSchema, 'HousingInformation');
 
